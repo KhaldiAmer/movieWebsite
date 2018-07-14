58cd036efb0d97054ab6e8a4c3eecefa55e86b12
Amer Khaldi <khaldi.ameur1995@gmail.com>
2018-07-14 21:23:50 +0100

    Update project.

--- website.py
+++ website.py
@@ -2,52 +2,18 @@ import media
 import fresh_tomatoes
 
 ant_man = media.Movie("Ant-man and the wasp",
-	"From the Marvel Cinematic Universe comes a new chapter featuring heroes with the astonishing ability to shrink: \"Ant-Man and The Wasp.\" In the aftermath of \"Captain America: Civil War,\" Scott Lang (Rudd) grapples with the consequences of his choices as both a Super Hero and a father. As he struggles to rebalance his home life with his responsibilities as Ant-Man, he's confronted by Hope van Dyne (Lilly) and Dr. Hank Pym (Douglas) with an urgent new mission. Scott must once again put on the suit and learn to fight alongside The Wasp as the team works together to uncover secrets from their past.",
 	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
 	"https://www.youtube.com/watch?v=8_rTIAOohas"
 	)
-movie2 = media.Movie("Ant-man and the wasp",
-	"From the Marvel Cinematic Universe comes a new chapter featuring heroes with the astonishing ability to shrink: \"Ant-Man and The Wasp.\" In the aftermath of \"Captain America: Civil War,\" Scott Lang (Rudd) grapples with the consequences of his choices as both a Super Hero and a father. As he struggles to rebalance his home life with his responsibilities as Ant-Man, he's confronted by Hope van Dyne (Lilly) and Dr. Hank Pym (Douglas) with an urgent new mission. Scott must once again put on the suit and learn to fight alongside The Wasp as the team works together to uncover secrets from their past.",
-	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
-	"https://www.youtube.com/watch?v=8_rTIAOohas"
+spider_man = media.Movie("Spider-Man: Into the Spider-Verse",
+	"https://upload.wikimedia.org/wikipedia/en/5/5e/Spider-Man_Into_the_Spider-Verse_poster.jpeg",
+	"https://youtu.be/g4Hbz2jLxvQ"
 	)
 
-movie3 = media.Movie("Ant-man and the wasp",
-	"From the Marvel Cinematic Universe comes a new chapter featuring heroes with the astonishing ability to shrink: \"Ant-Man and The Wasp.\" In the aftermath of \"Captain America: Civil War,\" Scott Lang (Rudd) grapples with the consequences of his choices as both a Super Hero and a father. As he struggles to rebalance his home life with his responsibilities as Ant-Man, he's confronted by Hope van Dyne (Lilly) and Dr. Hank Pym (Douglas) with an urgent new mission. Scott must once again put on the suit and learn to fight alongside The Wasp as the team works together to uncover secrets from their past.",
-	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
-	"https://www.youtube.com/watch?v=8_rTIAOohas"
+hotel_transylvania = media.Movie("Hotel Transylvania 3: Summer Vacation",
+	"https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Hotel_Transylvania_3_%282018%29_Poster.jpg/220px-Hotel_Transylvania_3_%282018%29_Poster.jpg",
+	"https://www.youtube.com/watch?v=Ku52zNnft8k"
 	)
 
-movie4 = media.Movie("Ant-man and the wasp",
-	"From the Marvel Cinematic Universe comes a new chapter featuring heroes with the astonishing ability to shrink: \"Ant-Man and The Wasp.\" In the aftermath of \"Captain America: Civil War,\" Scott Lang (Rudd) grapples with the consequences of his choices as both a Super Hero and a father. As he struggles to rebalance his home life with his responsibilities as Ant-Man, he's confronted by Hope van Dyne (Lilly) and Dr. Hank Pym (Douglas) with an urgent new mission. Scott must once again put on the suit and learn to fight alongside The Wasp as the team works together to uncover secrets from their past.",
-	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
-	"https://www.youtube.com/watch?v=8_rTIAOohas"
-	)
-
-movie5 = media.Movie("Ant-man and the wasp",
-	"From the Marvel Cinematic Universe comes a new chapter featuring heroes with the astonishing ability to shrink: \"Ant-Man and The Wasp.\" In the aftermath of \"Captain America: Civil War,\" Scott Lang (Rudd) grapples with the consequences of his choices as both a Super Hero and a father. As he struggles to rebalance his home life with his responsibilities as Ant-Man, he's confronted by Hope van Dyne (Lilly) and Dr. Hank Pym (Douglas) with an urgent new mission. Scott must once again put on the suit and learn to fight alongside The Wasp as the team works together to uncover secrets from their past.",
-	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
-	"https://www.youtube.com/watch?v=8_rTIAOohas"
-	)
-
-movie6 = media.Movie("Ant-man and the wasp",
-	"From the Marvel Cinematic Universe comes a new chapter featuring heroes with the astonishing ability to shrink: \"Ant-Man and The Wasp.\" In the aftermath of \"Captain America: Civil War,\" Scott Lang (Rudd) grapples with the consequences of his choices as both a Super Hero and a father. As he struggles to rebalance his home life with his responsibilities as Ant-Man, he's confronted by Hope van Dyne (Lilly) and Dr. Hank Pym (Douglas) with an urgent new mission. Scott must once again put on the suit and learn to fight alongside The Wasp as the team works together to uncover secrets from their past.",
-	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
-	"https://www.youtube.com/watch?v=8_rTIAOohas"
-	)
-
-movie7 = media.Movie("Ant-man and the wasp",
-	"From the Marvel Cinematic Universe comes a new chapter featuring heroes with the astonishing ability to shrink: \"Ant-Man and The Wasp.\" In the aftermath of \"Captain America: Civil War,\" Scott Lang (Rudd) grapples with the consequences of his choices as both a Super Hero and a father. As he struggles to rebalance his home life with his responsibilities as Ant-Man, he's confronted by Hope van Dyne (Lilly) and Dr. Hank Pym (Douglas) with an urgent new mission. Scott must once again put on the suit and learn to fight alongside The Wasp as the team works together to uncover secrets from their past.",
-	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
-	"https://www.youtube.com/watch?v=8_rTIAOohas"
-	)
-
-movie8 = media.Movie("Ant-man and the wasp",
-	"From the Marvel Cinematic Universe comes a new chapter featuring heroes with the astonishing ability to shrink: \"Ant-Man and The Wasp.\" In the aftermath of \"Captain America: Civil War,\" Scott Lang (Rudd) grapples with the consequences of his choices as both a Super Hero and a father. As he struggles to rebalance his home life with his responsibilities as Ant-Man, he's confronted by Hope van Dyne (Lilly) and Dr. Hank Pym (Douglas) with an urgent new mission. Scott must once again put on the suit and learn to fight alongside The Wasp as the team works together to uncover secrets from their past.",
-	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
-	"https://www.youtube.com/watch?v=8_rTIAOohas"
-	)
-
-
-movies = [ant_man, movie2, movie3, movie4, movie5, movie6, movie7, movie8]
+movies = [ant_man, spider_man, hotel_transylvania]
 fresh_tomatoes.open_movies_page(movies)
