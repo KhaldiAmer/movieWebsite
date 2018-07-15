e62caa5c6c27eb8118302ce62630ce04e96e9ec7
Amer Khaldi <khaldi.ameur1995@gmail.com>
2018-07-15 12:37:06 +0100

    Update project.

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 class Movie():
-	def __init__(self, movie_title, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+	"Stores movie information."
+	def __init__(self, title, poster_url, trailer_url):
+		self.title = title
+		self.poster_url = poster_url
+		self.trailer_url = trailer_url
 	
--- website.py
+++ website.py
@@ -1,16 +1,16 @@
-import media
+from movie import Movie
 import fresh_tomatoes
 
-ant_man = media.Movie("Ant-man and the wasp",
+ant_man = Movie("Ant-man and the wasp",
 	"http://cdn.collider.com/wp-content/uploads/2018/04/ant-man-and-the-wasp-poster.jpg",
 	"https://www.youtube.com/watch?v=8_rTIAOohas"
 	)
-spider_man = media.Movie("Spider-Man: Into the Spider-Verse",
+spider_man = Movie("Spider-Man: Into the Spider-Verse",
 	"https://upload.wikimedia.org/wikipedia/en/5/5e/Spider-Man_Into_the_Spider-Verse_poster.jpeg",
 	"https://youtu.be/g4Hbz2jLxvQ"
 	)
 
-hotel_transylvania = media.Movie("Hotel Transylvania 3: Summer Vacation",
+hotel_transylvania = Movie("Hotel Transylvania 3: Summer Vacation",
 	"https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Hotel_Transylvania_3_%282018%29_Poster.jpg/220px-Hotel_Transylvania_3_%282018%29_Poster.jpg",
 	"https://www.youtube.com/watch?v=Ku52zNnft8k"
 	)
